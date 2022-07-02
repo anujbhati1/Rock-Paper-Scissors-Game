cc668cb093791e3c4944cf774951c869ddcbec91
Anuj Bhati <anujbhati7488@gmail.com>
2022-07-02 23:14:57 +0530

    Rock-Paper-Scissors-Using-HTML-CSS-JS

---
+++ rps.js
@@ -0,0 +1,60 @@
+console.log("Everything is working fine.");
+
+let you;
+let yourScore = 0;
+let opponent;
+let opponentScore = 0;
+
+let choices = ["rock", "paper", "scissor"];
+
+window.onload = function () {
+  for (let i = 0; i < 3; i++) {
+    let choice = document.createElement("img");
+    choice.id = choices[i];
+    choice.src = choices[i] + ".png";
+    choice.addEventListener("click", selectChoice);
+    document.getElementById("choices").append(choice);
+  }
+};
+
+function selectChoice() {
+  you = this.id;
+  document.getElementById("your-choice").src = you + ".png";
+
+  //random for opponent
+
+  opponent = choices[Math.floor(Math.random() * 3)];
+  document.getElementById("opponent-choice").src = opponent + ".png";
+
+  //check for winner
+
+  if (you == opponent) {
+    yourScore += 1;
+    opponentScore += 1;
+  } else {
+    if (you == "rock") {
+      if (opponent == "scissor") {
+        yourScore += 1;
+      } else if (opponent == "paper") {
+        opponentScore += 1;
+      }
+    }
+    else if (you == "scissor") {
+      if (opponent == "paper") {
+        yourScore += 1;
+      } else if (opponent == "rock") {
+        opponentScore += 1;
+      }
+    }
+    else if (you == "paper") {
+      if (opponent == "rock") {
+        yourScore += 1;
+      } else if (opponent == "scissor") {
+        opponentScore += 1;
+      }
+    }
+  }
+
+  document.getElementById("your-score").innerText = yourScore;
+  document.getElementById("opponent-score").innerText = opponentScore;
+}
